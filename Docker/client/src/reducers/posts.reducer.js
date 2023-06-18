3c4d4c4f892016d06f6cdbc8a6491a6d1a4220c8
Bohdan Kletskyi <underwater3499@gmail.com>
2023-06-18 22:44:46 +0300

    docker

---
+++ Docker/client/src/reducers/posts.reducer.js
@@ -0,0 +1,97 @@
+import {createSlice, current} from "@reduxjs/toolkit";
+import axios from "axios";
+import setAuthToken from "../helpers/setAuthToken";
+
+const initialState = {
+    allPosts:[],
+    currentPost: null,
+}
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers:{
+        actionAllPosts:(state, {payload})=>{
+            state.allPosts = [...payload]
+        },
+        actionCurentPost:(state, {payload})=>{
+            state.currentPost = payload
+        }
+    }
+})
+
+export const {
+    actionAllPosts,
+    actionCurentPost,
+} = postsSlice.actions
+
+export const actionFetchAllPosts = ()=>(dispatch)=>{
+    const token = JSON.parse(JSON.stringify(localStorage.getItem('token')));
+    setAuthToken(token);
+    return axios
+    .get('http://localhost:3000/api/posts')
+    .then(({data, status})=>{
+        alert(status)
+        dispatch(actionAllPosts(data.result))
+    })
+    .catch((error) => {
+        alert(error) 
+      });
+}
+
+export const actionFetchCurentPost = (id)=>(dispatch)=>{
+    const token = JSON.parse(JSON.stringify(localStorage.getItem('token')));
+    setAuthToken(token);
+    return axios
+    .get(`http://localhost:3000/api/post/${id}`)
+    .then(({data, status})=>{
+        alert(status)
+        dispatch(actionCurentPost(data))
+    })
+    .catch((error) => {
+
+        alert(error) 
+      });
+}
+
+export const actionFetchCreateNewPost = (value)=>(dispatch)=>{ 
+    const token = JSON.parse(JSON.stringify(localStorage.getItem('token')));
+    setAuthToken(token);           
+    return axios   
+    .post(`http://localhost:3000/api/createNewPost`, value)
+    .then(({data, status})=>{
+        alert(status)
+    })
+    .catch((error) => {
+        alert(error.response.status)
+      });
+}
+
+export const actionFetchUpdatePost = (id, value)=>(dispatch)=>{
+    const token = JSON.parse(JSON.stringify(localStorage.getItem('token')));
+    setAuthToken(token);
+    return axios
+    .put(`http://localhost:3000/api/editpost/${id}`, value)
+    .then(({data, status})=>{
+        alert(status)
+        
+    })
+    .catch((error) => {
+        alert(error.response.status)
+      });
+}
+
+export const actionFetchDeletePost = (id)=>(dispatch)=>{
+    const token = JSON.parse(JSON.stringify(localStorage.getItem('token')));
+    setAuthToken(token);
+    return axios
+    .delete(`http://localhost:3000/api/delete/${id}`)
+    .then(({data, status})=>{
+        alert(status)
+    })
+    .catch((error) => {
+        alert(error) 
+      });
+}
+
+export default postsSlice.reducer;
---
+++ Docker/client/src/selectors/index.js
@@ -0,0 +1,5 @@
+export const selectorAllPosts = (state) => state.posts.allPosts
+export const selectorCurentPost = (state) => state.posts.currentPost
+export const selectorToken = (state) => state.users.token
+export const selectorUserEmail = (state) => state.users.userEmail
+export const selectorUserData = (state) => state.users.userData
